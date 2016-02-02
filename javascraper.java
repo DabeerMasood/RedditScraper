2a9018eb95817d08335cc5782ed6b4ec2b080bce
Dabeer M. Masood <dabeermasood@gmail.com>
2016-02-02 02:54:38 -0600

    working model

--- javascraper.java
+++ javascraper.java
@@ -1,12 +1,9 @@
-
-
 import java.io.IOException;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
@@ -16,145 +13,119 @@ import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
-
-import java.io.*;
-import java.net.URL;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import java.io.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import org.jsoup.Jsoup;
-
 import org.jsoup.nodes.Attributes;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
-import java.io.IOException;
-import java.net.URL;
 import java.util.Scanner;
+
+
+
 
 public class javascraper{
 
+    public static final String USER_AGENT = "USER AGENT";
+
+    
 public static void main (String[]args) throws MalformedURLException
 {
-	Scanner scan = new Scanner (System.in);
-	System.out.println("Where do you want to store the files?");
-	String folderpath = scan.next();
-	System.out.println("What subreddit do you want to scrape?");
-	String subreddit = scan.next();
-	subreddit = ("http://reddit.com/r/" + subreddit);
-	new File(folderpath + "/" + subreddit).mkdir();
-	
-//test
-
-try{
-    //gets http protocol
-Document doc = Jsoup.connect(subreddit).timeout(0).get();
-
-//get page title
-String title = doc.title();
-System.out.println("title : " + title);
-
-//get all links
-Elements links = doc.select("a[href]");
-
-for(Element link : links){
-
-//get value from href attribute
-String checkLink = link.attr("href");
-Elements images = doc.select("img[src~=(?i)\\.(png|jpe?g|gif)]");
-if (imgCheck(checkLink)){ // checks to see if img link j
-    System.out.println("link : " + link.attr("href"));
-downloadImages(checkLink, folderpath);
-
-
-
-
-
+    Scanner scan = new Scanner (System.in);
+    String choice = "";
+    String subreddit ="";
+    System.out.println("Where is your wallpapers folder?");
+    System.out.println("(Or any folder that you want to store images in)");
+    String folderpath = scan.next();
+    System.out.println("Do you want to download from r/wallpapers?");
+    choice = scan.nextLine();
+    if(choice.equals("Y") || choice.equals("y")){
+    subreddit = "wallpapers";
+    }else{
+    System.out.println("What subreddit do you want to scrape?");
+    subreddit = scan.next();
 }
+    subreddit = ("http://reddit.com/r/" + subreddit);
+    new File(folderpath + "/" + subreddit).mkdir();
+
+    //test
+
+    try{
+        //gets http protocol
+        Document doc = Jsoup.connect(subreddit).userAgent(USER_AGENT).timeout(0).get();
+
+
+        //get page title
+        String title = doc.title();
+        System.out.println("title : " + title);
+
+        //get all links
+        Elements links = doc.select("a[href]");
+
+        for(Element link : links){
+
+            //get value from href attribute
+            String checkLink = link.attr("href");
+            Elements images = doc.select("img[src~=(?i)\\.(png|jpe?g|gif)]");
+            if (imgCheck(checkLink)){ // checks to see if img link j
+                System.out.println("link : " + link.attr("href"));
+                downloadImages(checkLink, folderpath);
+            }
+        }
+    }
+    catch (IOException e){
+        e.printStackTrace();
+    }
 }
-
-
-
-}
-catch (IOException e){
-e.printStackTrace();
-}
-
-}
-
 
 public static boolean imgCheck(String http){
-String png = ".png";
-String jpg = ".jpg";
-String jpeg = "jpeg"; // no period so checker will only check last four characaters
-String gif = ".gif";
-int length = http.length();
+    String png = ".png";
+    String jpg = ".jpg";
+    String jpeg = "jpeg"; // no period so checker will only check last four characaters
+    String gif = ".gif";
+    int length = http.length();
 
-if (http.contains(png)|| http.contains("gfycat") || http.contains(jpg)|| http.contains(jpeg) || http.contains(gif)){
-return true;
+    if (http.contains(png)|| http.contains("gfycat") || http.contains(jpg)|| http.contains(jpeg) || http.contains(gif)){
+        return true;
+    }
+    else{
+        return false;
+    }
 }
-else{
-return false;
-}
-
-
-
-}
-
-
 
 private static void downloadImages(String src, String folderpath) throws IOException{
-String folder = null;
+    String folder = null;
 
-        //Exctract the name of the image from the src attribute
+    //Exctract the name of the image from the src attribute
 
-        int indexname = src.lastIndexOf("/");
+    int indexname = src.lastIndexOf("/");
 
-        if (indexname == src.length()) {
+    if (indexname == src.length()) {
+        src = src.substring(1, indexname);
+    }
+    indexname = src.lastIndexOf("/");
 
-            src = src.substring(1, indexname);
+    String name = src.substring(indexname, src.length());
 
-        }
- indexname = src.lastIndexOf("/");
+    System.out.println(name);
 
-        String name = src.substring(indexname, src.length());
+    //Open a URL Stream
+    URLConnection connection = (new URL(src)).openConnection();
 
-        System.out.println(name);
+    //Thread.sleep(2000); //Delay to comply with rate limiting
+    connection.setRequestProperty("User-Agent", USER_AGENT);
 
-        //Open a URL Stream
+    InputStream in = connection.getInputStream();
 
-        URL url = new URL(src);
+    OutputStream out = new BufferedOutputStream(new FileOutputStream( folderpath+ name));
 
-        InputStream in = url.openStream();
+    for (int b; (b = in.read()) != -1;) {
 
-        OutputStream out = new BufferedOutputStream(new FileOutputStream( folderpath+ name));
+        out.write(b);
 
- 
+    }
 
-        for (int b; (b = in.read()) != -1;) {
+    out.close();
 
-            out.write(b);
-
-        }
-
-        out.close();
-
-        in.close();
-
- 
-
-
-
-
+    in.close();
 }
 
-
-
 }
-
-
-
-
-
